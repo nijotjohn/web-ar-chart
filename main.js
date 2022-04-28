9baea95446b7131abb6356a4f41aa330a0429310
nijo.john <nijo.john@eqs.com>
2022-04-28 22:26:41 +0530

    ar test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import 'mind-ar/dist/mindar-image.prod.js';
-import 'aframe';
-import 'mind-ar/dist/mindar-image-aframe.prod.js';
-import 'mind-ar/dist/mindar-image-three.prod.js';
+// import 'mind-ar/dist/mindar-image.prod.js';
+// import 'aframe';
+// import 'mind-ar/dist/mindar-image-aframe.prod.js';
+// import 'mind-ar/dist/mindar-image-three.prod.js';
